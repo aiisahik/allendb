8d9d2c71f38dc428b0398b08b6d31cec67bd0ce4
aiisahik <jie.z.zhou@gmail.com>
2012-08-25 12:17:01 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Allendb::Application.config.secret_token = 'f9eb6402fd72c711e49f59f192f8232cec5ea077545a77fd751c2daacc16f977d63d2f52cf2f4e2b6dd9508043fcfb0bc5986867d4872cabace7c1700ae71a5d'
